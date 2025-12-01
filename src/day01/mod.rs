8deee07fbf9efbe1a122d6ac90fb4099d8ebdfa5
Martyna Jedwabna <martyna.jedwabna@icloud.com>
2025-12-01 21:44:16 +0100

    day01 part2

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,17 +1,33 @@
-use std::io::BufRead;
+use std::{io::BufRead};
 
 use itertools::Itertools;
 
 pub fn what_is_the_password_to_open_the_door(input: &mut dyn BufRead) -> usize {
-  let lines = crate::read_input(input);
-  let rotations = parse_input(lines);
+  let rotations = parse_input(crate::read_input(input));
   let mut dial = Dial::new();
 
   rotations
     .iter()
     .map(|r| dial.rotate(*r))
-    .filter(|p| *p == 0)
+    .filter(|(p, _)| *p == 0)
     .count()
+}
+
+pub fn what_is_the_password_to_open_the_door_using_password_method(
+  input: &mut dyn BufRead,
+) -> u32 {
+  let rotations = parse_input(crate::read_input(input));
+
+  how_many_times_did_dial_passed_zero(&rotations)
+}
+
+fn how_many_times_did_dial_passed_zero(rotations: &Vec<i32>) -> u32 {
+  let mut dial = Dial::new();
+  rotations
+    .iter()
+    .map(|r| dial.rotate(*r))
+    .map(|(_, passed_zero)| passed_zero)
+    .sum()
 }
 
 fn parse_input(lines: Vec<String>) -> Vec<i32> {
@@ -43,15 +59,31 @@ impl Dial {
     }
   }
 
-  fn rotate(&mut self, distance: i32) -> u32 {
-    self.position = ((self.position as i32 + distance).rem_euclid((self.max_number + 1) as i32)) as u32;
-    self.position
+  fn rotate(&mut self, distance: i32) -> (u32, u32) {
+    let before = self.position;
+    let temporary_position = self.position as i32 + distance;
+    self.position = ((temporary_position).rem_euclid((self.max_number + 1) as i32)) as u32;
+
+    let passed_zero_times = if distance > 0 {
+      (temporary_position / (self.max_number as i32 + 1)) as u32
+    }
+    else {
+      (temporary_position / (self.max_number as i32 + 1)).abs() as u32 + if temporary_position <= 0 && before != 0 { 1 as u32 } else {0 as u32}
+    };
+
+    (self.position, passed_zero_times)
   }
 }
 
 #[cfg(test)]
 mod tests {
-  use crate::{day01::what_is_the_password_to_open_the_door, read};
+  use crate::{
+    day01::{
+      what_is_the_password_to_open_the_door,
+      what_is_the_password_to_open_the_door_using_password_method,
+    },
+    read,
+  };
 
   #[test]
   fn sample_part1_input() {
@@ -60,12 +92,58 @@ mod tests {
       3
     );
   }
-  
+
   #[test]
   fn my_part1_input() {
     assert_eq!(
       what_is_the_password_to_open_the_door(&mut read("./src/day01/my.input")),
       1036
+    );
+  }
+
+  #[test]
+  fn sample_part2_input() {
+    assert_eq!(
+      what_is_the_password_to_open_the_door_using_password_method(&mut read(
+        "./src/day01/sample.input"
+      )),
+      6
+    );
+  }
+
+  #[test]
+  fn count_passed_zero() {
+    assert_eq!(super::Dial::new().rotate(49), (99, 0));
+    assert_eq!(super::Dial::new().rotate(50), (0, 1));
+    assert_eq!(super::Dial::new().rotate(51), (1, 1));
+    assert_eq!(super::Dial::new().rotate(-49), (1, 0));
+    assert_eq!(super::Dial::new().rotate(-50), (0, 1));
+    assert_eq!(super::Dial::new().rotate(-51), (99, 1));
+    assert_eq!(super::Dial::new().rotate(49 + 100), (99, 1));
+    assert_eq!(super::Dial::new().rotate(50 + 100), (0, 2));
+    assert_eq!(super::Dial::new().rotate(51 + 100), (1, 2));
+    assert_eq!(super::Dial::new().rotate(-49 - 100), (1, 1));
+    assert_eq!(super::Dial::new().rotate(-50 - 100), (0, 2));
+    assert_eq!(super::Dial::new().rotate(-51 - 100), (99, 2));
+    assert_eq!(super::Dial::new().rotate(49 + 100 * 2), (99, 2));
+    assert_eq!(super::Dial::new().rotate(50 + 100 * 2), (0, 3));
+    assert_eq!(super::Dial::new().rotate(-49 - 100 * 2), (1, 2));
+    assert_eq!(super::Dial::new().rotate(-50 - 100 * 2), (0, 3));
+  }
+
+  #[test]
+  fn count_passed_zero_multiple_rotations() {
+    assert_eq!(super::how_many_times_did_dial_passed_zero(&vec![-50, 1]), 1);
+    assert_eq!(super::how_many_times_did_dial_passed_zero(&vec![-50, -1]), 1);
+  }
+
+  #[test]
+  fn my_part2_input() {
+    assert_eq!(
+      what_is_the_password_to_open_the_door_using_password_method(&mut read(
+        "./src/day01/my.input"
+      )),
+      6228
     );
   }
 }
