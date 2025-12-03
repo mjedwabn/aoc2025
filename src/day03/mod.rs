72070ac96fd4312c8ac3e59a14dc9052fbb72a9f
Martyna Jedwabna <martyna.jedwabna@icloud.com>
2025-12-03 22:45:12 +0100

    feat: day 03 part 2

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -2,36 +2,45 @@ use std::io::BufRead;
 
 use crate::read_input;
 
-pub fn total_output_joltage(input: &mut dyn BufRead) -> i32 {
+type Long = u64;
+
+pub fn total_output_joltage(input: &mut dyn BufRead, digits: usize) -> Long {
   let banks = parse_banks(read_input(input));
-  banks.iter().map(|bank| find_largest_joltage(bank)).sum()
+  banks
+    .iter()
+    .map(|bank| find_max_joltage(bank, digits, 0, 0))
+    .sum()
 }
 
-fn parse_banks(lines: Vec<String>) -> Vec<Vec<i32>> {
+fn parse_banks(lines: Vec<String>) -> Vec<Vec<u32>> {
   lines
     .iter()
     .map(|line| {
       line
         .chars()
-        .map(|c| c.to_digit(10).unwrap() as i32)
+        .map(|c| c.to_digit(10).unwrap() as u32)
         .collect()
     })
     .collect()
 }
 
-fn find_largest_joltage(bank: &Vec<i32>) -> i32 {
-  let mut max_joltage = 0;
-  (0..bank.len() - 1).for_each(|i| {
-    bank[i + 1..bank.len()].iter().for_each(|b| {
-      let a = bank[i as usize];
-      let joltage = a * 10 + b;
-      if joltage > max_joltage {
-        max_joltage = joltage;
-      }
-    });
-  });
+fn find_max_joltage(bank: &Vec<u32>, digits: usize, digit: usize, start: usize) -> Long {
+  let max = bank[start..=bank.len() - digits + digit]
+    .iter()
+    .max()
+    .unwrap();
+  let index = bank
+    .iter()
+    .enumerate()
+    .position(|(i, n)| i >= start && n == max)
+    .unwrap();
 
-  max_joltage
+  *max as Long * 10u64.pow((digits as i32 - digit as i32 - 1) as u32)
+    + if digit + 1 == digits {
+      0
+    } else {
+      find_max_joltage(bank, digits, digit + 1, index + 1)
+    }
 }
 
 #[cfg(test)]
@@ -41,16 +50,32 @@ mod tests {
   #[test]
   fn sample_part1_input() {
     assert_eq!(
-      total_output_joltage(&mut read("./src/day03/sample.input")),
+      total_output_joltage(&mut read("./src/day03/sample.input"), 2),
       357
     );
   }
 
   #[test]
-  fn my_part1_input() {
+  fn sample_part1_input_generic() {
     assert_eq!(
-      total_output_joltage(&mut read("./src/day03/my.input")),
-      17229
+      total_output_joltage(&mut read("./src/day03/sample.input"), 2),
+      357
+    );
+  }
+
+  #[test]
+  fn sample_part2_input() {
+    assert_eq!(
+      total_output_joltage(&mut read("./src/day03/sample.input"), 12),
+      3121910778619
+    );
+  }
+
+  #[test]
+  fn my_part2_input() {
+    assert_eq!(
+      total_output_joltage(&mut read("./src/day03/my.input"), 12),
+      170520923035051
     );
   }
 }
