cc328a83d9ec206406987a46e39ae9c73a0d9b86
Martyna Jedwabna <martyna.jedwabna@icloud.com>
2025-12-07 23:47:54 +0100

    feat: day 07 part 2

--- src/day07/mod.rs
+++ src/day07/mod.rs
@@ -1,4 +1,4 @@
-use std::io::BufRead;
+use std::{collections::HashMap, io::BufRead};
 
 use crate::{CartesianGrid, Coords, GridCoords, read_input};
 
@@ -12,6 +12,81 @@ pub fn how_many_beam_splits(input: &mut dyn BufRead) -> usize {
   diagram.print();
 
   splits
+}
+
+fn how_many_different_timelines(input: &mut dyn BufRead) -> usize {
+  let lines = read_input(input);
+  let mut diagram = CartesianGrid::from(lines);
+
+  (0..diagram.height()).for_each(|y| {
+    diagram.move_beams(&y);
+  });
+
+  let mut map: HashMap<Coords, usize> = HashMap::<Coords, usize>::new();
+
+  (0..diagram.height() - 1).rev().for_each(|y| {
+    diagram
+      .coords_at_y(y)
+      .iter()
+      .filter(|c| *diagram.get(c) == '|')
+      .for_each(|c| {
+        map.entry(*c).or_insert(1);
+      });
+
+    diagram
+      .coords_at_y(y)
+      .iter()
+      .filter(|c| *diagram.get(c) == '|')
+      .for_each(|c| {
+        let this = *map.get(c).unwrap();
+
+        if let Some(splitter) = c
+          .sub_x(1)
+          .to_coords()
+          .filter(|l| l.in_grid(&diagram))
+          .filter(|l| *diagram.get(l) == '^')
+        {
+          map
+            .entry(splitter)
+            .and_modify(|count| *count += this)
+            .or_insert(this);
+        }
+
+        if let Some(splitter) = c
+          .add_x(1)
+          .to_coords()
+          .filter(|r| r.in_grid(&diagram))
+          .filter(|r| *diagram.get(r) == '^')
+        {
+          map
+            .entry(splitter)
+            .and_modify(|count| *count += this)
+            .or_insert(this);
+        }
+      });
+
+    diagram
+      .coords_at_y(y)
+      .iter()
+      .filter(|c| *diagram.get(c) == '|' || *diagram.get(c) == '^')
+      .for_each(|c| {
+        println!("From {:?} to {:?}", c, c.sub_y(1));
+        if let Some(beam) = c.sub_y(1).to_coords().filter(|above| {
+          *diagram.get(above) == '|' || *diagram.get(above) == '^' || *diagram.get(above) == 'S'
+        }) {
+          let this = *map.get(c).unwrap();
+          map
+            .entry(beam)
+            .and_modify(|count| *count += this)
+            .or_insert(this);
+        }
+      });
+  });
+
+  *diagram
+    .find_one_coords('S')
+    .map(|c| map.get(&c).unwrap())
+    .unwrap()
 }
 
 trait BeamDiagram {
@@ -78,7 +153,10 @@ impl BeamDiagram for CartesianGrid<char> {
 
 #[cfg(test)]
 mod tests {
-  use crate::{day07::how_many_beam_splits, read};
+  use crate::{
+    day07::{how_many_beam_splits, how_many_different_timelines},
+    read,
+  };
 
   #[test]
   fn sample_part1_input() {
@@ -93,6 +171,22 @@ mod tests {
     assert_eq!(
       how_many_beam_splits(&mut read("./src/day07/my.input")),
       1690
+    );
+  }
+
+  #[test]
+  fn sample_part2_input() {
+    assert_eq!(
+      how_many_different_timelines(&mut read("./src/day07/sample.input")),
+      40
+    );
+  }
+
+  #[test]
+  fn my_part2_input() {
+    assert_eq!(
+      how_many_different_timelines(&mut read("./src/day07/my.input")),
+      221371496188107
     );
   }
 }
